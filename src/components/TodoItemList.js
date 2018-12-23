a7cbee49c8f5804d9e45139060856d6e5cb2ca16
KIMINJOONG <ijboym216@gmail.com>
2018-12-23 11:41:59 +0900

    App.js, TodoItem.css TodoItemList.js 수정

--- src/components/TodoItemList.js
+++ src/components/TodoItemList.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
+import TodoItem from './TodoItem';
 
 class TodoItemList extends Component {
     render() 
     {
         const { todos, onToggle, onRemove } =this.props;
+
+        const todoList = todos.map(
+            (todo) => (
+                <TodoItem
+                    {...todo}
+                    onToggle={onToggle}
+                    onRemove={onRemove}
+                    key={todo.id}
+                />
+            )
+        );
         return(
             <div>
-
+                {todoList}
             </div>
 
         );
