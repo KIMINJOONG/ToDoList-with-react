aa9780b56d6f623f6cce48656e972f3787b6ab54
KIMINJOONG <ijboym216@gmail.com>
2019-01-13 13:32:37 +0900

    각 컴포넌트 스타일링하면서 추가

---
+++ src/components/TodoInput/TodoInput.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import styles from './TodoInput.scss';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
+
+const TodoInput = ({ value, onChange, onInsert}) => {
+    //Enter키가 눌리면 onInsert를 실행
+    const handleKeyPress = (e) => {
+        if(e.key === 'Enter') {
+            onInsert();
+        }
+    }
+    return(
+        <div className={cx('todo-input')}>
+            <input onChange={onChange} value={value} onKeyPress={handleKeyPress} />
+            <div className={cx('add-button')} onClick={onInsert}>추가</div>
+        </div>
+        
+    );
+};
+
+export default TodoInput;
